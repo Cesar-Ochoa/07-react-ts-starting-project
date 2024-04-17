905a59220611293631230b6024e81a86a5808907
Cesar-Ochoa <cesar.ochoacalvillo@gmail.com>
2024-04-17 07:27:09 -0600

    Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Todo text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Todo text/i);
   expect(linkElement).toBeInTheDocument();
 });
